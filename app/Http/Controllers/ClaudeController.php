2f051c682bfcb872ea4ebe591bf0f08d0151b173
Artur Hanusek <artur@coding.cab>
2025-08-02 21:18:41 +0200

    rebuild

--- app/Http/Controllers/ClaudeController.php
+++ app/Http/Controllers/ClaudeController.php
@@ -4,14 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Services\ClaudeService;
 use Exception;
+use Illuminate\Http\Request;
 
 class ClaudeController extends Controller
 {
     /**
      * @throws Exception
      */
-    public function store($request)
+    public function store(Request $request)
     {
-        return ClaudeService::stream(request('command'));
+        return ClaudeService::stream(request('prompt'), request('options', '--permission-mode bypassPermissions'));
     }
 }
