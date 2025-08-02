1e08d51fdedf797126890fd8e315e190cf0665ed
Artur Hanusek <artur@coding.cab>
2025-08-02 20:56:49 +0200

    rebuild

---
+++ app/Services/ClaudeService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services;
+
+class ClaudeService
+{
+    public function stream(string $prompt, string $options = '--permission-mode bypassPermissions')
+    {
+        throw new \Exception('Not implemented');
+    }
+}
