2994e433605f0919ec3a80a9123c310b2e88a771
Artur Hanusek <artur@coding.cab>
2025-08-09 14:05:03 +0200

    refactoring

--- app/Jobs/SendClaudeMessageJob.php
+++ app/Jobs/SendClaudeMessageJob.php
@@ -9,14 +9,13 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\File;
 
 class SendClaudeMessageJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    protected $conversation;
-    protected $message;
+    protected Conversation $conversation;
+    protected string $message;
 
     public function __construct(Conversation $conversation, string $message)
     {
@@ -26,18 +25,6 @@ class SendClaudeMessageJob implements ShouldQueue
 
     public function handle(): void
     {
-        $baseDirectory = storage_path('app/private/repositories/base/' . $this->conversation->repository);
-        $hotDirectory = storage_path('app/private/repositories/hot/' . $this->conversation->repository);
-        $projectDirectory = storage_path('app/private/repositories/projects/' . $this->conversation->project_directory);
-
-        if (File::exists($hotDirectory)) {
-            File::moveDirectory($hotDirectory, $projectDirectory);
-        } else {
-            File::copyDirectory($baseDirectory, $projectDirectory);
-        }
-
-        PrepareProjectDirectoryJob::dispatch($this->conversation);
-
         ClaudeService::stream(
             $this->message,
             '--permission-mode bypassPermissions',
